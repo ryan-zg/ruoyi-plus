6b6a09bec0471fc9616cdf5483d0cc78fdeff597
Ryan/Rui.Zhang <3399289091@qq.com>
2024-11-28 20:58:41 +0800

    前端添加测试页面，后端添加测试代码用户检测mybatis-plus是否添加成功

---
+++ manage/src/main/java/com/ruoyi/manage/config/MybatisPlusConfig.java
@@ -0,0 +1,9 @@
+package com.ruoyi.manage.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.ruoyi.manage.mapper")
+public class MybatisPlusConfig {
+} 
